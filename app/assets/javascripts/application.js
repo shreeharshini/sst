1549ec68dfe4ca96e04c8685e0e42b15221fe39e
Kirti <kirtitrikha09@gmail.com>
2016-12-20 12:12:31 +0530

    remove conflicts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-ui
-//= require autocomplete-rails
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
