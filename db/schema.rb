8b0744f82f79c302b9a6944923ab3f08a9e937d6
Aditya <aditya.padole@mps-in.com>
2016-12-28 15:18:45 +0530

    dynamic

--- db/schema.rb
+++ db/schema.rb
@@ -394,12 +394,6 @@ ActiveRecord::Schema.define(version: 20161228063446) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "fyears", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-
   create_table "generated_reports", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
     t.integer  "report_id"
