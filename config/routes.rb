4e47470e44c95c92cbd0c671ed4f105d1019371f
shreeharshini <mamidalaharshini@gmail.com>
2017-01-02 17:51:29 +0530

    graphs

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
 
  
-  get 'counter_and_custom_reports/index'
-
-  get 'counter_and_custom_reports/csv'
-
-  get 'counter_and_custom_reports/xlsx'
-
-  get 'counter_and_custom_reports/zip'
-
 
   get 'export_files/csv'
 
@@ -16,45 +8,64 @@ Rails.application.routes.draw do
 
   get 'export_files/zip'
 
-  get 'support/contactus'
+ 
 
-  get 'support/faq'
+  resources :support do 
+    collection do
+      get 'contactus'
+      get 'faq'
+      get 'aboutus'
+    end
+  end
 
-  get 'support/aboutus'
+  resources :search_by_issn do 
+    collection do 
+      get 'search'
+      get 'load_suggestions'
+      get 'show_report'
+    end
+  end
 
-	get "loginpage/dynamicreports"
-	get "loginpage/sourcereports"
-	get "loginpage/report"
-  get "loginpage/accessdetails"
-  get "loginpage/selectedplatforms"
 	get "home/index"
-  get "integration_partners/manage_sushi_account"
-  get "integration_partners/update_permission"
   get "import_data/xlsx"
-  get "search_by_issn/search"
-  get "search_by_issn/load_suggestions"
+ 
 
-  # resources :products do
-  #   get :autocomplete_brand_name, :on => :collection
-  # end
+  resources :integration_partners do 
+    collection do
+      get 'manage_sushi_account'
+      get 'update_permission'
+    end
+
+  end
+
+  resources :counter_and_custom_reports do 
+    collection do
+      get 'show_reports_by_year'
+      get 'csv'
+      get 'xlsx'
+      get 'zip'
+    end
+  end
 
   resources :loginpage do
-  collection do
-    get 'getyear'
-    get 'getreports'
-    get 'test2'
-   end
-end
-  get "counter_and_custom_reports/show"
-  get "counter_and_custom_reports/show_reports_by_year"
-  get "search_by_issn/show_report"
+    collection do
+      get 'getyear'
+      get 'getreports'
+      get 'test2'
+      get 'dynamicreports'
+      get 'sourcereports'
+      get 'accessdetails'
+      get 'selectedplatforms'
+     end
+  end
+ 
+ get 'year_trends/highpiecharts'
   
-  resources :accounts, :reports, :platforms, 
-            :source_reports_mappings, :year_trends, :year_usages,
-            :year_top_journals, :account_infos, :export_files, 
-            :platform_reports, :requestfordemos, :contact_us, 
-            :library_account_details, :import_data, :search_by_issn , 
-            :integration_partners, :counter_and_custom_reports, :additional_users
+  resources :accounts, :reports, :platforms, :source_reports_mappings, 
+            :year_trends, :year_usages, :year_top_journals, :account_infos, 
+            :export_files, :platform_reports, :requestfordemos, :contact_us, 
+            :library_account_details, :import_data,  
+            :additional_users
 
   
 
