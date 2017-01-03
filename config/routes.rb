2c68a7f0d7b18debad8b932e3572aeff3330214e
Kirti <kirtitrikha09@gmail.com>
2017-01-03 12:17:58 +0530

    remove conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,17 @@
 Rails.application.routes.draw do
 
  
-<<<<<<< HEAD
-  get 'counter_and_custom_reports/index'
-  get 'counter_and_custom_reports/csv'
-  get 'counter_and_custom_reports/xlsx'
-  get 'counter_and_custom_reports/zip'
-  get "counter_and_custom_reports/show"
-  get "counter_and_custom_reports/show_reports_by_year"
-=======
->>>>>>> 370ffcd60f8b8c88629b5db4fa50d04ef229783f
+  # get 'counter_and_custom_reports/index'
+  # get 'counter_and_custom_reports/csv'
+  # get 'counter_and_custom_reports/xlsx'
+  # get 'counter_and_custom_reports/zip'
+  # get "counter_and_custom_reports/show"
+  # get "counter_and_custom_reports/show_reports_by_year"
 
-  get 'export_files/csv'
-  get 'export_files/xlsx'
-  get 'export_files/zip'
+  # get 'export_files/csv'
+  # get 'export_files/xlsx'
+  # get 'export_files/zip'
 
-<<<<<<< HEAD
   get 'support/contactus'
   get 'support/faq'
   get 'support/aboutus'
@@ -30,14 +26,14 @@ Rails.application.routes.draw do
   get "users/index"
   get "users/sign_out"
 
-  get "integration_partners/manage_sushi_account"
-  get "integration_partners/update_permission"
+  # get "integration_partners/manage_sushi_account"
+  # get "integration_partners/update_permission"
 
-  get "import_data/xlsx"
+  #get "import_data/xlsx"
 
-  get "search_by_issn/search"
-  get "search_by_issn/load_suggestions"
-  get "search_by_issn/show_report"
+  # get "search_by_issn/search"
+  # get "search_by_issn/load_suggestions"
+  # get "search_by_issn/show_report"
 
   post "users/create"
   # resources :products do
@@ -53,9 +49,7 @@ Rails.application.routes.draw do
 end
   
   
-  resources :accounts, :users, :reports, :platforms,
-=======
- 
+  resources :accounts, :users, :reports, :platforms
 
   resources :support do 
     collection do
@@ -87,6 +81,13 @@ end
 
   end
 
+  resources :export_files do
+    collection do
+      get 'csv'
+      get 'xlsx'
+      get 'zip'
+    end
+  end
   resources :counter_and_custom_reports do 
     collection do
       get 'show_reports_by_year'
@@ -112,7 +113,7 @@ end
   
 
   resources :accounts, :reports, :platforms,
->>>>>>> 370ffcd60f8b8c88629b5db4fa50d04ef229783f
+
             :source_reports_mappings, :year_trends, :year_usages,
             :year_top_journals, :account_infos, :export_files, 
             :platform_reports, :requestfordemos, :contact_us, 
