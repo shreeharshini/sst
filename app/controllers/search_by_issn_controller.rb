f223380ac2a95e7a783dc79f186993ea0c9282b0
Kirti <kirtitrikha09@gmail.com>
2017-01-13 18:29:13 +0530

    search_by_issn

--- app/controllers/search_by_issn_controller.rb
+++ app/controllers/search_by_issn_controller.rb
@@ -1,5 +1,5 @@
 class SearchByIssnController < ApplicationController
-	autocomplete :data_library, :print_issn,:full => true
+	#autocomplete :data_library, :print_issn,:full => true
 	def index
 		#@data_library = DataLibrary.all
 	end
@@ -13,13 +13,21 @@ class SearchByIssnController < ApplicationController
 	end
 
 	def search
-
-		@results =DataLibrary.where("print_issn LIKE '%#{params[:issn]}%'")
+		if params[:select_type] == "JournalPrintISSN"
+			@results =DataLibrary.where("print_issn LIKE '%#{params[:issn]}%'")
+		elsif params[:select_type] == "journalOnlineISSN"
+			@results =DataLibrary.where("online_issn LIKE '%#{params[:issn]}%'")
+		elsif params[:select_type] == "bookISBN"
+			@results =DataLibrary.where("isbn LIKE '%#{params[:issn]}%'")
+		else params[:select_type] == "bookISSN"
+			@results =DataLibrary.where("issn LIKE '%#{params[:issn]}%'")
+		end
+		#@results = DataLibrary.where(section:  params[:select_type], year: params[:year] ? params[:year] : "2016")
+		#@results =DataLibrary.where("print_issn LIKE '%#{params[:issn]}%'")
 		# @results = ReportSection.where(section: "counter" , year: params[:year] ? params[:year] : "2016")
 	end
 
 	def load_suggestions
-		
 		if params[:select_type] == "JournalPrintISSN"
 			@suggestion = DataLibrary.pluck(:print_issn).last(10)
 		elsif params[:select_type] == "journalOnlineISSN"
@@ -40,7 +48,7 @@ class SearchByIssnController < ApplicationController
   	end
 
   	def get_cost_per_usage
-  		@cost_per_usage = params[:usage].to_i / 2
+  		@cost_per_usage = params[:type].to_i / params[:usage].to_i
   		respond_to do |format|
 			format.json {render json: @cost_per_usage.to_json}
 		end
